af6ae03f783b440381e008362e51ce6ea1694e6b
afesl <afesl@student.tgm.ac.at>
2017-09-26 13:06:20 +0200

    First commit.

---
+++ src/HelloOptions.java
@@ -0,0 +1,15 @@
+
+public class HelloOptions {
+	
+	public static void Hello1() {
+		System.out.println("Hallo1");
+	}
+	
+	public static void Hello2() {
+		System.out.println("Hallo2");
+	}
+	
+	public static void Hello3() {
+		System.out.println("Hallo3");
+	}
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		HelloOptions.Hello1();
+		HelloOptions.Hello2();
+		HelloOptions.Hello3();
+	}
+}
