dea987ef05ee1ce800f985638ebb52e77d8a01d3
afesl <afesl@student.tgm.ac.at>
2017-09-26 13:19:13 +0200

    rufzeichen hinzufügen

--- src/HelloOptions.java
+++ src/HelloOptions.java
@@ -2,14 +2,14 @@
 public class HelloOptions {
 	
 	public static void Hello1() {
-		System.out.println("Hallo1");
+		System.out.println("Hallo1 !!!!!!!!!!!");
 	}
 	
 	public static void Hello2() {
-		System.out.println("Hallo2");
+		System.out.println("Hallo2 !!!!!!!!");
 	}
 	
 	public static void Hello3() {
-		System.out.println("Hallo3");
+		System.out.println("Hallo3!!!!!!!!!!!");
 	}
 }
